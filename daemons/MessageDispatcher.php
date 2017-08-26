1ef401fc2adc73308401ecd0249d6e5ae7879fab
LAPTOP-GLBPT2V4\malitha <ncmalitha@gmail.com>
2017-08-27 02:46:50 +0300

    adding php script that watches messages
    This acts as a queue for messages

---
+++ daemons/MessageDispatcher.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: malitha
+ * Date: 8/27/2017
+ * Time: 1:00 AM
+ */
+
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+require_once dirname(__FILE__) . '/../app/Models/Message.php';
+require_once dirname(__FILE__) . '/../app/MessageClients/MessageBird.php';
+
+use Models\Message;
+use MessageClients\MessageBird;
+
+// Since cron job will run only every 60 seconds min
+while (true) {
+
+
+    $message       = new \Models\Message();
+    $queuedMessage = $message->fetchMessageFromQueue();
+
+    if($queuedMessage) {
+
+        try {
+
+            $messageBirdClient = new \MessageClients\MessageBird();
+            if ($messageBirdClient->hasCredit()) {
+                $response = $messageBirdClient->sendMessage($queuedMessage['recipient'], $queuedMessage['originator'],
+                    $queuedMessage['message']);
+
+                if($response and $response->recipients->totalSentCount > 0) {
+                    $message->updateStatus($queuedMessage['id'], 'DELIVERED');
+                    $message->updateAttempts($queuedMessage['id'], $queuedMessage['attempts'] + 1);
+                }else {
+                    $message->updateAttempts($queuedMessage['id'], $queuedMessage['attempts'] + 1);
+                }
+            }
+
+        }catch( Exception $e) {
+            $message->updateAttempts($queuedMessage['id'], $queuedMessage['attempts'] + 1);
+        }
+
+
+    }
+
+    sleep(1);
+
+}
